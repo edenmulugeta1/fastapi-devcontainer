88d30ad67739cf0e95e7271a18f391091d112153
edenmulugeta1 <157423101+edenmulugeta1@users.noreply.github.com>
2024-10-29 10:43:01 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -21,4 +21,12 @@ def customer (idx:int):
     df = pd.read_csv("../cutomers.csv")
     customer = df.iloc[idx]
     return customer.to_dict()
-    
+
+@api.post("/get_body")
+async def get_body(request: Request):
+    return = await request.json()
+    first_name = response["fname"]
+    last_name = response["lname"]
+    favorite_number = response["favnu]
+    return {"first_name": first_name, "last_name": last_name, "favorite_number": favorite_name}
+
